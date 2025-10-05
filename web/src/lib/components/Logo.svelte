14c54e15880298a12c27167c3aab3c5c73e92d04
Megakuul <linus.moser@megakuul.ch>
2025-10-06 00:32:17 +0200

    feat: add initial logo

---
+++ web/src/lib/components/Logo.svelte
@@ -0,0 +1,75 @@
+<div class="flex justify-center items-center p-4 rounded-2xl bg-slate-100 backdrop-blur-3xl ">
+  <svg width="466" height="469" viewBox="0 0 466 469" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <g filter="url(#filter0_i_8_59)">
+      <path id="outer" d="M455.517 454.93C449.633 461.187 10.1438 464.922 7.39739 454.93C4.65096 444.938 203.131 378.256 211.58 366.666C220.028 355.076 5.88681 26.3928 9.62171 17.6785C13.3566 8.96422 214.825 14.3987 214.825 14.3987C214.825 14.3987 450.919 5.97184 453.75 10.5798C456.581 15.1878 271.587 110.311 271.33 121.887C271.074 133.464 461.401 448.673 455.517 454.93Z" stroke="url(#paint0_linear_8_59)" stroke-width="9"/>
+      <path id="center" d="M421.886 437.347C417.462 441.984 86.9266 444.651 84.8587 437.243C82.7908 429.834 233.38 383.425 239.732 374.834C246.084 366.244 40.0697 39.9921 42.8766 33.5324C45.6836 27.0727 197.208 31.1487 197.208 31.1487C197.208 31.1487 374.77 24.9563 376.901 28.3732C379.031 31.7901 239.92 102.268 239.73 110.851C239.54 119.434 426.31 432.71 421.886 437.347Z" stroke="url(#paint1_linear_8_59)" stroke-width="9"/>
+      <path id="inner" d="M393.451 422.424C390.473 425.547 167.909 427.342 166.517 422.354C165.125 417.365 263.271 388.778 267.548 382.993C271.825 377.209 76.818 57.4041 78.708 53.0545C80.598 48.705 182.625 51.4495 182.625 51.4495C182.625 51.4495 302.185 47.2799 303.62 49.5806C305.054 51.8813 211.385 99.3373 211.257 105.116C211.129 110.895 396.43 419.302 393.451 422.424Z" stroke="url(#paint2_linear_8_59)" stroke-width="9"/>
+    </g>
+    
+    <defs>
+      <filter id="filter0_i_8_59" x="2.86572" y="4.70203" width="457.285" height="464.9" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+      <feFlood flood-opacity="0" result="BackgroundImageFix"/>
+      <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape"/>
+      <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha"/>
+      <feOffset dy="4"/>
+      <feGaussianBlur stdDeviation="2"/>
+      <feComposite in2="hardAlpha" operator="arithmetic" k2="-1" k3="1"/>
+      <feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"/>
+      <feBlend mode="normal" in2="shape" result="effect1_innerShadow_8_59"/>
+      </filter>
+      <linearGradient id="paint0_linear_8_59" x1="237.639" y1="4.94947" x2="227.675" y2="463.287" gradientUnits="userSpaceOnUse">
+      <stop stop-color="#747474"/>
+      <stop offset="1" stop-color="#929292"/>
+      </linearGradient>
+      <linearGradient id="paint1_linear_8_59" x1="236.987" y1="24.6404" x2="227.893" y2="442.997" gradientUnits="userSpaceOnUse">
+      <stop stop-color="#B4B3B3"/>
+      <stop offset="1" stop-color="#828282"/>
+      </linearGradient>
+      <linearGradient id="paint2_linear_8_59" x1="240.221" y1="47.737" x2="232.007" y2="425.558" gradientUnits="userSpaceOnUse">
+      <stop stop-color="#8F8F8F"/>
+      <stop offset="1" stop-color="#8E8E8E"/>
+      </linearGradient>
+    </defs>
+  </svg>
+  <svg viewBox="0 0 466 469" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path id="outer" d="M455.517 454.93C449.633 461.187 10.1438 464.922 7.39739 454.93C4.65096 444.938 203.131 378.256 211.58 366.666C220.028 355.076 5.88681 26.3928 9.62171 17.6785C13.3566 8.96422 214.825 14.3987 214.825 14.3987C214.825 14.3987 450.919 5.97184 453.75 10.5798C456.581 15.1878 271.587 110.311 271.33 121.887C271.074 133.464 461.401 448.673 455.517 454.93Z" />
+    <path id="center" d="M421.887 437.347C417.463 441.984 86.9267 444.651 84.8588 437.242C82.7909 429.834 233.38 383.425 239.732 374.834C246.084 366.244 40.0698 39.992 42.8767 33.5324C45.6837 27.0727 197.208 31.1487 197.208 31.1487C197.208 31.1487 374.771 24.9562 376.901 28.3731C379.031 31.79 239.92 102.268 239.73 110.851C239.54 119.433 426.31 432.71 421.887 437.347Z" />
+    <path id="inner" d="M393.451 422.424C390.472 425.547 167.909 427.342 166.517 422.354C165.125 417.365 263.271 388.778 267.548 382.993C271.825 377.209 76.8179 57.4041 78.7079 53.0545C80.5979 48.705 182.625 51.4495 182.625 51.4495C182.625 51.4495 302.185 47.2799 303.62 49.5806C305.054 51.8813 211.385 99.3373 211.257 105.116C211.129 110.895 396.43 419.302 393.451 422.424Z" />
+  </svg>
+</div>
+
+<style>
+  #outer {
+    stroke-dasharray: 2200;
+    stroke-dashoffset: 2200;
+    stroke-linecap: round;
+    stroke-linejoin: round;
+    animation: animate-layer 2.5s forwards ease;
+  }
+
+  #center {
+    stroke-dasharray: 1800;
+    stroke-dashoffset: -1800;
+    stroke-linecap: round;
+    stroke-linejoin: round;
+    animation: animate-layer 2.5s forwards ease;
+  }
+
+  #inner {
+    stroke-dasharray: 1500;
+    stroke-dashoffset: 1500;
+    stroke-linecap: round;
+    stroke-linejoin: round;
+    animation: animate-layer 2s .5s forwards ease;
+  }
+
+  @keyframes animate-layer {
+    0% {
+      stroke-opacity: 0.5;
+    }
+    100% {
+      stroke-opacity: 1;
+      stroke-dashoffset: 0;
+    }
+  }
+</style>
--- web/src/routes/+page.svelte
+++ web/src/routes/+page.svelte
@@ -1,2 +1,9 @@
+<script>
+  import Logo from "../lib/components/Logo.svelte";
+
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+<Logo></Logo>
